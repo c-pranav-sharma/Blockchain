692a7a8585015cb0dba1c01063cd09d38dfcb410
Chouduru Pranav Sharma <145117301+c-pranav-sharma@users.noreply.github.com>
2026-05-08 17:55:08 +0530

    Implement AttendanceToken contract with rewards

--- Week-2/AttendanceToken.sol
+++ Week-2/AttendanceToken.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
+
+contract AttendanceToken is ERC20, Ownable {
+
+    event AttendanceRewarded(address indexed attendee, uint256 amount);
+
+    constructor() ERC20("Attendance Token", "AT") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function rewardAttendance(address attendee, uint256 amount) public onlyOwner {
+        _mint(attendee, amount);
+
+        emit AttendanceRewarded(attendee, amount);
+    }
+}
